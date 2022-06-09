6cb7ba8135ed08ed9cd98276567d8d0e497f0086
Roxanne Weber <89050736+roxanneweber@users.noreply.github.com>
2022-06-09 12:17:17 -0500

    all routes but image increment are working

---
+++ server.js
@@ -0,0 +1,101 @@
+import express from "express";
+const app = express();
+
+// code needed to make __dirname and __filename work in ES modules
+import * as url from "url";
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+//
+
+const database = {
+	users: [
+		{
+			id: "123",
+			name: "John",
+			email: "john@gmail.com",
+			password: "cookies",
+			entries: 0,
+			joined: new Date(),
+		},
+		{
+			id: "124",
+			name: "Sally",
+			email: "sally@gmail.com",
+			password: "bananas",
+			entries: 0,
+			joined: new Date(),
+		},
+	],
+};
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(express.static(__dirname + "public"));
+
+// ROUTES
+
+// /  root -- res = 'server is working'
+app.get("/", (req, res) => {
+	res.send(database.users);
+});
+
+// /signin -- POST res = success/fail
+app.post("/signin", (req, res) => {
+	if (
+		req.body.email === database.users[0].email &&
+		req.body.password === database.users[0].password
+	) {
+		res.json("successful signin");
+	} else {
+		res.status(400).json("error logging in");
+	}
+});
+// /register -- POST res = user
+app.post("/register", (req, res) => {
+	const { email, name, password } = req.body;
+	database.users.push({
+		id: "125",
+		name: name,
+		email: email,
+		password: password,
+		entries: 0,
+		joined: new Date(),
+	});
+	res.json(database.users[database.users.length - 1]);
+});
+
+// /profile:userId -- GET res = user profile
+app.get("/profile/:id", (req, res) => {
+	const { id } = req.params;
+	let found = false;
+	database.users.forEach((user) => {
+		if (user.id === id) {
+			found = true;
+			return res.json(user);
+		}
+	});
+	if (!found) {
+		res.status(400).json("user not found");
+	}
+});
+
+// /image -- PUT res = updated user object or count
+app.post("/image", (req, res) => {
+	const { id } = req.body;
+	let found = false;
+	database.users.forEach((user) => {
+		if (user.id === id) {
+			found = true;
+			user.entries++;
+			return res.json(user.entries);
+		}
+	});
+	if (!found) {
+		res.status(400).json("user not found");
+	}
+});
+
+app.listen(9000, () => {
+	console.log("Listening on PORT 9000");
+});
