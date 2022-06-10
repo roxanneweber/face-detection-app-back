602d059c6d0d67606b1797bb806d48936808d0e1
Roxanne Weber <89050736+roxanneweber@users.noreply.github.com>
2022-06-09 23:51:25 -0500

    signin route passing to db correctly

--- server.js
+++ server.js
@@ -33,27 +33,6 @@ import * as url from "url";
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 //
 
-const database = {
-	users: [
-		{
-			id: "123",
-			name: "John",
-			email: "john@gmail.com",
-			password: "cookies",
-			entries: 0,
-			joined: new Date(),
-		},
-		{
-			id: "124",
-			name: "Sally",
-			email: "sally@gmail.com",
-			password: "bananas",
-			entries: 0,
-			joined: new Date(),
-		},
-	],
-};
-
 // middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -69,23 +48,34 @@ app.get("/", (req, res) => {
 
 // /signin -- POST res = success/fail
 app.post("/signin", (req, res) => {
-	bcrypt.hash(password, saltRounds, function (err, hash) {
-		// Store hash in your password DB.
-	});
-
-	if (
-		req.body.email === database.users[0].email &&
-		req.body.password === database.users[0].password
-	) {
-		res.json(database.users[0]);
-	} else {
-		res.status(400).json("error logging in");
-	}
+	bcrypt;
+	// .hash(password, saltRounds, function (err, hash) {
+	// Store hash in your password DB.
+	db.select("email", "hash")
+		.from("login")
+		.where("email", "=", req.body.email)
+		.then((data) => {
+			const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+			if (isValid) {
+				return db
+					.select("*")
+					.from("users")
+					.where("email", "=", req.body.email)
+					.then((user) => {
+						res.json(user[0]);
+					})
+					.catch((err) => res.status(400).json("unable to get user"));
+			} else {
+				res.status(400).json("wrong credentials");
+			}
+		})
+		// })
+		.catch((err) => res.status(400).json("wrong credentials"));
 });
+
 // /register -- POST res = user
 app.post("/register", (req, res) => {
 	const { email, name, password } = req.body;
-
 	const hash = bcrypt.hash(password, saltRounds, function (err, hash) {
 		// Store hash in your password DB.
 		db.transaction((trx) => {
